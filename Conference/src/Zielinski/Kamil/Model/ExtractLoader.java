7d5fff5b31ae6a6ebd8e9638fb014444734ec36d
kzielin4 <kamilpiotrlukaszzielinski@gmail.com>
2016-08-24 15:20:09 +0200

    Add Class Validator

--- Conference/src/Zielinski/Kamil/Model/ExtractLoader.java
+++ Conference/src/Zielinski/Kamil/Model/ExtractLoader.java
@@ -1,6 +1,24 @@
 package Zielinski.Kamil.Model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExtractLoader
 {
-	
+	public void read(String file)
+	{
+		try (BufferedReader br = new BufferedReader(new FileReader(file)))
+		{
+			String line;
+			while ((line = br.readLine()) != null)
+			{
+				System.out.println(line);
+			}
+		}
+		catch (IOException e)
+		{
+			System.out.println("CAN NOT OPEN THE PATH");
+		}
+	}
 }
---
+++ Conference/src/Zielinski/Kamil/Model/Validator.java
@@ -0,0 +1,10 @@
+package Zielinski.Kamil.Model;
+
+public class Validator
+{
+	
+	public int validate(String line)
+	{
+		return 1;
+	}
+}
