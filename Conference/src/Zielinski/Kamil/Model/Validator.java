44340442974867480654550e44d0f62deb34497a
kzielin4 <kamilpiotrlukaszzielinski@gmail.com>
2016-08-24 16:40:52 +0200

    Add empty function declaration to Validator

--- Conference/src/Zielinski/Kamil/Model/Validator.java
+++ Conference/src/Zielinski/Kamil/Model/Validator.java
@@ -1,10 +1,30 @@
 package Zielinski.Kamil.Model;
 
+import java.sql.Date;
+
+import javax.xml.crypto.Data;
+
 public class Validator
 {
-	
+
 	public int validate(String line)
 	{
 		return 1;
 	}
+
+	public int validateName(String name)
+	{
+		return 1;
+	}
+
+	public int validateLastName(String lastName)
+	{
+		return 1;
+	}
+
+	public int validateDates(Data arrival, Date departure)
+	{
+		return 1;
+	}
+
 }
