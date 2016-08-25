023d20e4ed796eea56c3c0229dab92997bebcabd
kzielin4 <kamilpiotrlukaszzielinski@gmail.com>
2016-08-25 14:13:34 +0200

    Add field to Logger

--- Conference/src/Zielinski/Kamil/Model/Logger.java
+++ Conference/src/Zielinski/Kamil/Model/Logger.java
@@ -2,5 +2,24 @@ package Zielinski.Kamil.Model;
 
 public class Logger
 {
-	String logLine;
+	public Logger(String logLine, String path)
+	{
+		super();
+		this.logLine = logLine;
+		this.path = path;
+	}
+	private String logLine;
+    private String path;
+	public void writeLog(String line)
+	{
+
+	}
+	public String getPath()
+	{
+		return path;
+	}
+	public void setPath(String path)
+	{
+		this.path = path;
+	}
 }
