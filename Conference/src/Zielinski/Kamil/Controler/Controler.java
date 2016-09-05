d39b24d1316b6fc15b644a3e11e7898a13339a93
kzielin4 <kamilpiotrlukaszzielinski@gmail.com>
2016-09-05 09:52:43 +0200

    Add JDBC connector and View FXML

--- Conference/src/Zielinski/Kamil/Controler/Controler.java
+++ Conference/src/Zielinski/Kamil/Controler/Controler.java
@@ -1,13 +1,29 @@
 package Zielinski.Kamil.Controler;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Controler
+public class Controler extends Application
 {
-   private Button button;
-   public void click()
-   {
-	   System.out.println("Kliknalem");
-   }
-   
+	private Button button;
+
+	public void click()
+	{
+		System.out.println("Kliknalem");
+	}
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+
+	}
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
 }
---
+++ Conference/src/Zielinski/Kamil/Controler/Main.java
@@ -0,0 +1,41 @@
+package Zielinski.Kamil.Controler;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		Application.launch(Main.class, (java.lang.String[]) null);
+	}
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+			Pane page = (Pane) FXMLLoader.load(Main.class.getResource("View.fxml"));
+			Scene scene = new Scene(page);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Conference");
+			primaryStage.show();
+			primaryStage.setResizable(false);
+		}
+		catch (Exception ex)
+		{
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
